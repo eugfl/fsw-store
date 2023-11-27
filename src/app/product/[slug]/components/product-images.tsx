b0d9e805e22bf925fa9f7b88f8d36fad8d9c0f13
Gabriel Figueiredo <eugfl@hotmail.com>
2023-11-27 14:49:04 -0300

    chore: simplify styles arguments to classes with TailwindCSS

--- src/app/product/[slug]/components/product-images.tsx
+++ src/app/product/[slug]/components/product-images.tsx
@@ -1,19 +1,15 @@
 "use client";
-
 import Image from "next/image";
 import { useState } from "react";
-
 interface ProductImagesProps {
   name: string;
   imageUrls: string[];
 }
-
 const ProductImages = ({ imageUrls, name }: ProductImagesProps) => {
   const [currentImage, setCurrentImage] = useState(imageUrls[0]);
   const handleImageClick = (imageUrl: string) => {
     setCurrentImage(imageUrl);
   };
-
   return (
     <div className="flex flex-col">
       <div className="flex h-[380px] w-full items-center justify-center bg-accent">
@@ -23,21 +19,20 @@ const ProductImages = ({ imageUrls, name }: ProductImagesProps) => {
           height={0}
           width={0}
           sizes="100vw"
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-          style={{ objectFit: "contain" }}
+          className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
         />
       </div>
 
-      <div className="mt-8 grid grid-cols-4 gap-4 px-5 ">
+      <div className="mt-8 grid grid-cols-4 gap-4 px-5">
         {imageUrls.map((imageUrl) => (
           <button
             key={imageUrl}
-            className={`bg-accent" flex h-[100px] items-center justify-center rounded-lg
+            className={`flex h-[100px] items-center justify-center rounded-lg bg-accent
                 ${
                   imageUrl === currentImage &&
                   "border-2 border-solid border-primary"
                 }
-              `}
+            `}
             onClick={() => handleImageClick(imageUrl)}
           >
             <Image
@@ -54,5 +49,4 @@ const ProductImages = ({ imageUrls, name }: ProductImagesProps) => {
     </div>
   );
 };
-
 export default ProductImages;
